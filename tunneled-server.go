bf1c1f1a6023fb3630b16d8ee201f765ebd65994
Brooks Swinnerton <bswinnerton@gmail.com>
2016-12-17 19:30:56 -0500

    Try new approach with channels and requests

--- tunneled-server.go
+++ tunneled-server.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"fmt"
-	log "github.com/Sirupsen/logrus"
-	"golang.org/x/crypto/ssh"
-	"io"
 	"io/ioutil"
 	"net"
 	"os"
 	"os/exec"
-	"strings"
-)
 
-const (
-	privateHostKeyPath = "./tunneled.master.key"
-	port               = "2222"
+	log "github.com/Sirupsen/logrus"
+	"golang.org/x/crypto/ssh"
 )
 
 func init() {
@@ -22,8 +16,40 @@ func init() {
 	log.SetLevel(log.DebugLevel)
 }
 
-func handleServerConn(keyID string, chans <-chan ssh.NewChannel) {
-	log.Info("SSH: Begin handling server connection...")
+const (
+	privateHostKeyPath = "./tunneled.master.key"
+	port               = "2222"
+)
+
+type forwardChannelArgs struct {
+	Addr       string
+	Port       uint32
+	OriginAddr string
+	OriginPort uint32
+}
+
+type Server struct {
+	Config *ssh.ServerConfig
+	Port   string
+}
+
+func findOrCreateHostKey() ssh.Signer {
+	if _, err := os.Stat(privateHostKeyPath); os.IsNotExist(err) {
+		log.Info("SSH: Host key does not exist, creating...")
+		createHostKey()
+	}
+
+	hostKeyBytes, err := ioutil.ReadFile(privateHostKeyPath)
+	if err != nil {
+		log.Panic("SSH: Failed to load host's private key")
+	}
+
+	hostKey, err := ssh.ParsePrivateKey(hostKeyBytes)
+	if err != nil {
+		log.Panic("SSH: Failed to parse host's private key")
+	}
+
+	return hostKey
 }
 
 func createHostKey() {
@@ -32,73 +58,104 @@ func createHostKey() {
 	err := cmd.Run()
 
 	if err != nil {
-		panic(fmt.Sprintf("SSH: Failed to create private key for host %s", err))
+		log.Panic(fmt.Sprintf("SSH: Failed to create private key for host %s", err))
 	}
-
-	log.Info("SSH: New private host key generated: " + privateHostKeyPath)
 }
 
-func loadPrivateHostKey() ssh.Signer {
-	privateHostKeyBytes, err := ioutil.ReadFile(privateHostKeyPath)
+func authorizeByPublicKey(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
+	// TODO: Perform lookup to see if key is known, find associated user.
+	//formattedKey := strings.TrimSpace(string(ssh.MarshalAuthorizedKey(key)))
+
+	return &ssh.Permissions{}, nil
+}
+
+func (server *Server) Start() error {
+	log.Info("Starting server...")
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
+
 	if err != nil {
-		panic("SSH: Failed to load host's private key")
+		return err
 	}
 
-	privateHostKey, err := ssh.ParsePrivateKey(privateHostKeyBytes)
-	if err != nil {
-		panic("SSH: Failed to parse host's private key")
+	defer listener.Close()
+
+	for {
+		tcpConn, err := listener.Accept()
+		if err != nil {
+			log.Panic(fmt.Sprintf("Failed to accept incoming connection (%s)", err))
+		}
+
+		sshConn, chans, reqs, err := ssh.NewServerConn(tcpConn, server.Config)
+		if err != nil {
+			log.Panic(fmt.Sprintf("Failed to handshake (%s)", err))
+		}
+
+		log.Info(fmt.Sprintf("New SSH connection from %s (%s)", sshConn.RemoteAddr(), sshConn.ClientVersion()))
+
+		go handleRequests(reqs)
+		go handleChannels(chans, sshConn)
+	}
+}
+
+func handleRequests(reqs <-chan *ssh.Request) {
+	for req := range reqs {
+		if req.Type == "tcpip-forward" {
+			req.Reply(true, []byte{})
+		} else {
+			log.Warn("got unexpected request %q WantReply=%q: %q\n", req.Type, req.WantReply, req.Payload)
+			req.Reply(false, nil)
+		}
+	}
+}
+
+func handleChannels(chans <-chan ssh.NewChannel, conn *ssh.ServerConn) {
+	for newChannel := range chans {
+		go handleChannel(newChannel, conn)
+	}
+}
+
+func handleChannel(newChannel ssh.NewChannel, conn *ssh.ServerConn) {
+	channelType := newChannel.ChannelType()
+
+	if channelType != "direct-tcpip" {
+		newChannel.Reject(ssh.Prohibited, "direct-tcpip channels only (-NR)")
+		log.Info(fmt.Sprintf("Rejecting SSH connection for %s@%s: didn't pass -NR flags", conn.User(), conn.RemoteAddr()))
+		return
 	}
 
-	return privateHostKey
+	args := forwardChannelArgs{}
+	err := ssh.Unmarshal(newChannel.ExtraData(), &args)
+	if err != nil {
+		log.Warning(fmt.Sprintf("Failed to parse channel request data: %s", err))
+		newChannel.Reject(ssh.Prohibited, "invalid request data")
+		return
+	}
+
+	connection, requests, err := newChannel.Accept()
+	if err != nil {
+		log.Panic(fmt.Sprintf("Could not accept channel (%s)", err))
+		return
+	}
+
+	for req := range requests {
+		log.Info(req.Type)
+	}
+
+	connection.Close()
 }
 
 func main() {
 	config := &ssh.ServerConfig{
-		PublicKeyCallback: func(conn ssh.ConnMetadata, key ssh.PublicKey) (*ssh.Permissions, error) {
-			formattedPrivateKey := strings.TrimSpace(string(ssh.MarshalAuthorizedKey(key)))
-			log.Debug("User's private key is: " + formattedPrivateKey)
-
-			return &ssh.Permissions{Extensions: map[string]string{"key-id": "SUCCESS"}}, nil
-		},
+		PublicKeyCallback: authorizeByPublicKey,
 	}
 
-	if _, err := os.Stat(privateHostKeyPath); os.IsNotExist(err) {
-		log.Info("SSH: Host key does not exist, creating...")
-		createHostKey()
+	hostKey := findOrCreateHostKey()
+	config.AddHostKey(hostKey)
+
+	server := &Server{
+		Config: config,
+		Port:   port,
 	}
 
-	privateHostKey := loadPrivateHostKey()
-
-	config.AddHostKey(privateHostKey)
-
-	log.Info("SSH: Listening...")
-	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Error(fmt.Sprintf("SSH: Error accepting incoming connection: %v", err))
-		}
-
-		go func() {
-			log.Info("SSH: Handshaking for %s", conn.RemoteAddr())
-			sConn, chans, reqs, err := ssh.NewServerConn(conn, config)
-			if err != nil {
-				if err == io.EOF {
-					log.Warn(fmt.Sprintf("SSH: Handshaking was terminated: %v", err))
-				} else {
-					log.Error(fmt.Sprintf("SSH: Error on handshaking: %v", err))
-				}
-				return
-			}
-
-			log.Info(fmt.Sprintf("SSH: Connection from %s (%s)", sConn.RemoteAddr(), sConn.ClientVersion()))
-			go ssh.DiscardRequests(reqs)
-			go handleServerConn(sConn.Permissions.Extensions["key-id"], chans)
-		}()
-	}
+	server.Start()
 }
