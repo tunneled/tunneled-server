807da36fa081c3cb63ac41aa2657b7d972c59c39
Brooks Swinnerton <bswinnerton@gmail.com>
2016-12-23 19:30:28 -0500

    Add introductory comments

--- tunneled-server.go
+++ tunneled-server.go
@@ -1,9 +1,62 @@
-// Notes
-// https://github.com/pivotal-cf-experimental/remote-pairing-release/blob/master/src/github.com/pivotal-cf-experimental/ssh-tunnel/server.go#L298
-// https://github.com/Sirupsen/logrus
-// https://github.com/emulbreh/sshub/blob/c14f516babcc121ae62de2ada5ebffd779e4d6b6/libsshub/hub.go
-// https://github.com/Kane-Sendgrid/wormhole/blob/53cd61266020a26a2464439885560f8cf11b9d24/ssh.go#L180
-// ssh -NR 8001:localhost:8000 brooks@localhost -p 2222
+/*
+
+Tunneled Server
+
+This application multiplexes SSH tunnels, proxying HTTP requests to the
+appropriate tunnel based on the subdomain of the request. It looks something
+like this:
+
+                  ┏━━━━━━━━┓                                          ┏━━━━━━━━┓
+                  ┃        ┃                                          ┃        ┃
+                  ┃        ┃ ━┓                                    ┏━ ┃        ┃
+ ─────Request───▶ ┃        ┃  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛  ┃        ┃
+                  ┃Tunneled┃ ───────────────────────────────────────▶ ┃Tunneled┃
+                  ┃ Server ┃ ◀─────────────────────────────────────── ┃ Client ┃
+ ◀────Response─── ┃        ┃  ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓  ┃        ┃
+                  ┃        ┃ ━┛                                    ┗━ ┃        ┃
+                  ┃        ┃                                          ┃        ┃
+                  ┗━━━━━━━━┛                                          ┗━━━━━━━━┛
+
+A standard SSH tunnel connects the server and tunnel. HTTP requests bound for a
+*.tunneled.computer domain are received by the server, and passed on to the
+appropriate client based on the subdomain the user has claimed. Zooming into the
+above diagram, we see:
+
+                    ┌──────────────────────────────────────┐  ┌─────────────────
+                    │                                      └──┘    Tunnel to
+                    │                                      ┌──┐ Alice's Computer
+                    │  ┌──────────────┐  ┌────────────┐    │  └─────────────────
+                    │  │              │  │            │    │  ┌─────────────────
+                    │  │  Request     │  │            │ ┌─▶└──┘    Tunnel to
+───────Request─────▶├┐ │  Director    │  │            │─┘  ┌──┐ Brooks' Computer
+                    ││ │              │  │ SSH Server │ ┌──│  └─────────────────
+                    │└▶│1. HTTP Parser├─▶│            │◀┘  │  ┌─────────────────
+                    │  │              │  │1. Auth     │    └──┘    Tunnel to
+                    │  │2. Subdomain  │  │            │    ┌──┐ Cat's Computer
+                    │ ┌┤   Lookup     │◀─┤2. Tunnel DB│    │  └─────────────────
+                    │ ││              │  │            │    │  ┌─────────────────
+◀──────Response─────┤◀┘│3. Tunnel     │  │            │    └──┘    Tunnel to
+                    │  │   Connection │  │            │    ┌──┐ Otto's Computer
+                    │  │              │  │            │    │  └─────────────────
+                    │  └──────────────┘  └────────────┘    │  ┌─────────────────
+                    │                                      └──┘    Tunnel to
+                    │                                      ┌──┐ Luna's Computer
+                    └──────────────────────────────────────┘  └─────────────────
+
+This application consists of two parts:
+
+1. The SSH Server: responsible for establishing connections with SSH clients. It
+   handles authentication based on the public keys defined in the `users.json`
+   file and stores established connections in an in-memory store. Tunnels are
+   lost when the server dies.
+
+2. The Request Director: responsible for handling incoming HTTP requests to the
+   server, determining the subdomain, and then passing along the HTTP request to
+   the appropriate tunnel. Any responses coming from the tunnel are proxied back
+   through the server to the requester.
+
+
+*/
 
 package main
 
