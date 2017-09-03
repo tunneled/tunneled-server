bdb5f875dab6225f63bece98d8dd4b208ae3d7e5
Brooks Swinnerton <bswinnerton@gmail.com>
2017-09-03 19:14:59 -0400

    Process incoming connections in goroutines

--- request_director.go
+++ request_director.go
@@ -28,87 +28,91 @@ func (director *RequestDirector) Start() {
 	log.Infof("Request director listening on port %s", director.port)
 
 	for {
-		request, err := listener.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Warnf("Could not accept connection: %s", err)
 			continue
 		}
 
-		var requestBuf bytes.Buffer
-		requestReader := io.TeeReader(request, &requestBuf)
-
-		httpRequest, err := http.ReadRequest(bufio.NewReader(requestReader))
-		if err != nil {
-			log.WithFields(log.Fields{
-				"remote_ip": request.RemoteAddr(),
-			}).Warnf("Couldn't parse request as HTTP: %s", err)
-
-			if err := request.Close(); err != nil {
-				log.Warnf("Could not close request", err)
-			}
-
-			continue
-		}
-
-		domain := httpRequest.Host
-
-		contextLogger := log.WithFields(log.Fields{
-			"remote_ip":          request.RemoteAddr(),
-			"destination_domain": domain,
-			"destination_path":   httpRequest.URL.Path,
-		})
-
-		contextLogger.Info("Incoming request")
-
-		if director.port != "80" {
-			domain, _, err = net.SplitHostPort(domain)
-			if err != nil {
-				contextLogger.Warnf("Could not split host and port: %s", err)
-			}
-		}
-
-		tun, err := director.FindTunnel(domain)
-		if err != nil {
-			contextLogger.Info(err)
-			director.Handle404(request)
-			continue
-		}
-
-		sshChannel, err := director.sshServer.createChannel(*tun)
-		if err != nil {
-			contextLogger.Warnf("Couldn't create a tunnel for: http://%s", domain)
-
-			director.Handle404(request)
-			continue
-		}
-
-		go func() {
-			_, err := io.Copy(sshChannel, &requestBuf)
-			if err != nil {
-				contextLogger.Warnf("Couldn't copy request to tunnel: %s", err)
-				return
-			}
-
-			if err = sshChannel.CloseWrite(); err != nil {
-				contextLogger.Warnf("Could not close SSH channel: %s", err)
-			}
-
-			_, err = io.Copy(request, sshChannel)
-			if err != nil {
-				contextLogger.Warnf("Couldn't copy response from tunnel: %s", err)
-				return
-			}
-
-			contextLogger.Info("Returned response")
-
-			if err = request.Close(); err != nil {
-				contextLogger.Warnf("Could not close request: %s", err)
-			}
-		}()
+		go director.handleConn(conn)
 	}
 }
 
-func (director *RequestDirector) FindTunnel(domain string) (*Tunnel, error) {
+func (director *RequestDirector) handleConn(conn net.Conn) {
+	var connBuf bytes.Buffer
+	connReader := io.TeeReader(conn, &connBuf)
+
+	httpRequest, err := http.ReadRequest(bufio.NewReader(connReader))
+	if err != nil {
+		log.WithFields(log.Fields{
+			"remote_ip": conn.RemoteAddr(),
+		}).Warnf("Couldn't parse request as HTTP: %s", err)
+
+		if err := conn.Close(); err != nil {
+			log.Warnf("Could not close request", err)
+		}
+
+		return
+	}
+
+	domain := httpRequest.Host
+
+	contextLogger := log.WithFields(log.Fields{
+		"remote_ip":          conn.RemoteAddr(),
+		"destination_domain": domain,
+		"destination_path":   httpRequest.URL.Path,
+	})
+
+	contextLogger.Info("Incoming request")
+
+	if director.port != "80" {
+		domain, _, err = net.SplitHostPort(domain)
+		if err != nil {
+			contextLogger.Warnf("Could not split host and port: %s", err)
+		}
+	}
+
+	tun, err := director.findTunnel(domain)
+	if err != nil {
+		contextLogger.Info(err)
+		director.handle404(conn)
+		return
+	}
+
+	sshChannel, err := director.sshServer.createChannel(*tun)
+	if err != nil {
+		contextLogger.Warnf("Couldn't create a tunnel for: http://%s", domain)
+
+		director.handle404(conn)
+		return
+	}
+
+	go func() {
+		_, err := io.Copy(sshChannel, &connBuf)
+		if err != nil {
+			contextLogger.Warnf("Couldn't copy request to tunnel: %s", err)
+			return
+		}
+
+		if err = sshChannel.CloseWrite(); err != nil {
+			contextLogger.Warnf("Could not close SSH channel: %s", err)
+		}
+
+		_, err = io.Copy(conn, sshChannel)
+		if err != nil {
+			contextLogger.Warnf("Couldn't copy response from tunnel: %s", err)
+			return
+		}
+
+		contextLogger.Info("Returned response")
+
+		if err = conn.Close(); err != nil {
+			contextLogger.Warnf("Could not close request: %s", err)
+		}
+	}()
+}
+
+func (director *RequestDirector) findTunnel(domain string) (*Tunnel, error) {
 	director.sshServer.RLock()
 	tun := director.sshServer.tunnels[domain]
 	director.sshServer.RUnlock()
@@ -120,7 +124,7 @@ func (director *RequestDirector) FindTunnel(domain string) (*Tunnel, error) {
 	return tun, nil
 }
 
-func (director *RequestDirector) Handle404(request net.Conn) {
+func (director *RequestDirector) handle404(request net.Conn) {
 	bodyBuf := bytes.NewBufferString("No tunnel found.")
 	response := http.Response{
 		StatusCode:    404,
